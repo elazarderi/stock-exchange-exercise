10cfa894ee3cdc6c76614653afe04403b2e00a50
elazar deri <elazarderi@gmail.com>
2022-08-30 22:11:35 +0300

    base routing

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
-console.log('Hello world!')
-
+import express from "express";
+import path from "path";
 import mongoose, { ConnectOptions } from 'mongoose';
 import * as dotenv from 'dotenv';
+import { router } from "./routes";
 
 dotenv.config({ path: __dirname + '/../.env' });
+
+const app = express();
+const port = process.env.SERVER_PORT;
+
+app.use(express.json());
+
+app.use('/', router);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// start the express server
+app.listen(port, () => {    
+    console.log( `server started at http://localhost:${port}`);
+});
 
 mongoose.connect(process.env.MONGO_URI!, { useNewUrlParser: true } as ConnectOptions)
     .then(() => console.log('success')).catch((err) => console.log(err));
